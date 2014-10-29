7cf32c36fc63c565c2faab07784309246b6c3378
Tobias Downer <toby@mckoi.com>
2014-10-29 16:16:30 -0700

    Remaining width calculated incorrectly when floating area is initially placed outside of the current line bounds. Added special case calculation to deal with this situation.

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/layout/InlineBoxing.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/layout/InlineBoxing.java
@@ -366,6 +366,17 @@ public class InlineBoxing {
                              c, currentLine, child, remainingWidth, pendingFloats);
                     // Preserve lineStart for floating elements,
                     lineStart = oldLineStart;
+                    // Handle special case where the flow element can be placed
+                    // outside of the current line bounds, so we need to
+                    // recalculate the area we have to work with.
+                    if (remainingWidth <= 0) {
+                        // Calculate the new width bounds. This must take into
+                        // account the situation where the float element is placed
+                        // far from the distance of the originating line.
+                        BlockFormattingContext bFContext = c.getBlockFormattingContext();
+                        float floatDistance = bFContext.getFloatDistance(c, currentLine, maxAvailableWidth);
+                        remainingWidth = maxAvailableWidth - (int) Math.ceil(floatDistance);
+                    }
                 } else if (child.getStyle().isInlineBlock() || child.getStyle().isInlineTable()) {
                     layoutInlineBlockContent(c, box, child, initialY);
 
